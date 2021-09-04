b4cafd91810dc1691297a10fc2166402d06cf16b
kevinfiol <me@kevinfiol.com>
2021-09-03 23:18:08 -0400

    comments; remove ITAD ad-tracking from GOG urls

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sys, os
 from dotenv import load_dotenv
 from bottle import route, redirect, run, request, static_file, template, TEMPLATES
+from urllib.parse import unquote
 from ITAD import ITAD
 
 load_dotenv()
@@ -36,6 +37,10 @@ def results():
     if game_name.strip() == '':
         redirect('/')
 
+    # `plain_id` is an input element that is added dynamically via javascript in `app.js`
+    # `plain_id` is only passed when the user selects from the autocomplete dropdown
+    # if `plain_id` is set, that means we can skip searching
+    # because we essentially have already searched and retrieved a `plain_id` that ITAD understands
     if not plain_id:
         search_results = itad.search(game_name.strip())
         search_results = search_results['data']['results']
@@ -49,6 +54,15 @@ def results():
         res = itad.getPrices(plain_id)
         prices = res['data']
 
+    # remove ad tracker from gog URL
+    gog_price_idx = findIndex(lambda x: x['shop']['id'] == 'gog', prices)
+
+    if gog_price_idx != -1:
+        gog_price = prices[gog_price_idx]
+        endpoint = unquote(gog_price['url'].split('www.gog.com')[1]) # unquote decodes uri components
+        url = 'https://www.gog.com' + endpoint
+        gog_price['url'] = url
+
     return template('results',
         game_exists=game_exists,
         game_name=game_name,
@@ -60,5 +74,12 @@ def results():
 @route('/search/<name>')
 def search(name):
     return itad.search(name.strip())
+
+# findIndex util
+def findIndex(fn, xs):
+    for i, v in enumerate(xs):
+        if fn(v):
+            return i
+    return -1
 
 run(host=os.getenv('HOST'), port=os.getenv('PORT'))
